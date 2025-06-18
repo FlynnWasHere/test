ea2944bba799e389abf40c5eeeb5d21b401cc2d3
Flynn <GenevieveJohnson4444@gmail.com>
2025-06-18 00:24:49 -0700

    Update main.php

--- main.php
+++ main.php
@@ -1,8 +1,117 @@
-<?php $title = '1914'; include("top.php");?>
-//This would be where you would make the changes that need to be made on each page.
+<html>
+  <html lang="en">
+    
+ <head>
+   
+    <title>Fandoms</title>
+    <link rel="icon" type="image/x-icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9APxkj0xClmrU3PpMZglHQkx446nQPG6lA&s">
+   
+    <meta charset="utf-8">
+ 		<meta name="viewport" content="width=device-width, initial-scale=1">
+ 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+ 	  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+ 	  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+ 	  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+   
+  <link rel="stylesheet" type="text/css" href="https://flynnwashere.github.io/SilksongTimeLine/main.css">
+  
+   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+   
+ </head>
+    
+    
+    
+<body>
 
-<article>
-<br style="clear: both" />
-</article>
+<!--Navigation bar-->
+<div id="nav-placeholder">
 
-<?php include("bottom.php");?>
+</div>
+
+<script>
+$(function(){
+  $("#nav-placeholder").load("https://flynnwashere.github.io/test/top.html");
+});
+</script>
+<!--end of Navigation bar-->
+  
+
+  
+  
+<div class="main">
+  <h1>welcome</h1>
+  <h5>gaze upon the beauty of this creation</h5>
+  <p>if your on mobile...sorry...</p>
+  
+  <article>
+  <h3>huh</h3>
+  <p>First we silk and then we song</p>
+  <p>let's...</p>
+  </article>
+  
+  <br>
+  
+  <h2>timeline</h2>
+  <br>
+  
+  <div class="container"> <!--this container has all the stuf fin the timeline. when adding new content, just remmeber-2 columsn per row.-->
+    
+    
+    <div class="row">
+      <div class="col-lg timeline-item">
+        <article class="">
+    			<img src="https://media.tenor.com/Pp_6LGxxzlAAAAAM/cat-cat-meme.gif" class="timeline-image">
+    			<div class="timeline-text">
+            <h4>A Title</h4>
+   			  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla venenatis placerat. Ut leo enim, pretium quis dictum vitae, varius ultrices elit. Aliquam ut ex nisl. In hac habitasse platea dictumst. I</p>
+		  		</div>
+  		</article>
+      </div> <!--/col-->
+      
+      <div class="col-lg timeline-item">
+        <article class="">
+    			<img src="https://media.tenor.com/Pp_6LGxxzlAAAAAM/cat-cat-meme.gif" class="timeline-image">
+    			<div class="timeline-text">
+            <h4>A Title</h4>
+   			  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla venenatis placerat. Ut leo enim, pretium quis dictum vitae, varius ultrices elit. Aliquam ut ex nisl. In hac habitasse platea dictumst. I</p>
+		  		</div>
+  		</article>
+      </div> <!--/col-->
+      
+    </div><!--/row-->
+    
+    <div class="row">
+      <div class="col-lg timeline-item">
+        <article class="">
+    			<img src="https://media.tenor.com/Pp_6LGxxzlAAAAAM/cat-cat-meme.gif" class="timeline-image">
+    			<div class="timeline-text">
+            <h4>A Title</h4>
+   			  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla venenatis placerat. Ut leo enim, pretium quis dictum vitae, varius ultrices elit. Aliquam ut ex nisl. In hac habitasse platea dictumst. I</p>
+		  		</div>
+  		</article>
+      </div> <!--/col-->
+      
+      <div class="col-lg timeline-item">
+        <article class="">
+    			<img src="https://media.tenor.com/Pp_6LGxxzlAAAAAM/cat-cat-meme.gif" class="timeline-image">
+    			<div class="timeline-text">
+            <h4>A Title</h4>
+   			  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla venenatis placerat. Ut leo enim, pretium quis dictum vitae, varius ultrices elit. Aliquam ut ex nisl. In hac habitasse platea dictumst. I</p>
+		  		</div>
+  		</article>
+      </div> <!--/col-->
+      
+    </div><!--/row-->
+    
+  </div><!--/container-->
+  
+  
+  <br>
+  
+  
+</div> <!--main end-->
+ 
+  
+</body>
+    
+<html>
